e058ce827eeb8c6a3b5aa6f33de594c4062243b2
Weijun Yin <gw9812@gmail.com>
2014-11-20 16:42:30 +0800

    refactor test to avoid a repetitive new bucket setup

--- test/kv/bucket_test.exs
+++ test/kv/bucket_test.exs
@@ -1,8 +1,13 @@
 defmodule KV.BucketTest do
   use ExUnit.Case, async: true
+  
+  setup do
+    {:ok, pid} = KV.Bucket.start_link() 
+    {:ok, bucket: pid}
+  end
 
-  test "store values by key" do
-    {:ok, pid} = KV.Bucket.start_link()
+  test "store values by key", %{bucket: pid} do
+
     assert KV.Bucket.get(pid, :zhi) == nil
 
     KV.Bucket.put(pid, :zhi, 2)
